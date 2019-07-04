3d0b868ec3a5b40f475b447767f7871db376414f
Raul Dipeas <rauldipeas@mail.com>
2019-07-04 11:33:59 -0300

    GitLab CI

--- download-counter.sh
+++ download-counter.sh
@@ -1,9 +1,9 @@
 #!/bin/#!/usr/bin/env bash
-sudo apt update;sudo apt install -y git python-requests wget
+apt update;apt install -y git python-requests wget
 wget -q https://raw.githubusercontent.com/mmilidoni/github-downloads-count/master/gdc;chmod +x gdc
 ./gdc rauldipeas radix>downloads.log ; tac downloads.log | awk 'NR <=1' | sed 's/	Total Downloads for repo rauldipeas\/radix//g' | echo `xargs expr 4000 + ` Downloads > download-counter.log
 git clone https://github.com/rauldipeas/radix-website;cd radix-website
 cp ../download-counter.log website/download-counter/download-counter.log
 git config --global user.email "rauldipeas@mail.com";git config --global user.name "Raul Dipeas"
-git add .; git commit -m "Azure Pipeline Auto Build"
+git add .; git commit -m "GitLab CI Auto Build"
 git push http://rauldipeas:$GITHUB_ACCESS_TOKEN@github.com/rauldipeas/radix-website.git HEAD:master
