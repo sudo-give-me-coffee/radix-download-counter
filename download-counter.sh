90e40ee65dedde7edb6ca74109bf7fc9423dc27d
Natanael <natanaelgplay@gmail.com>
2019-07-12 22:42:28 -0300

    Deixa a linha visível em uma tela só no editor

--- download-counter.sh
+++ download-counter.sh
@@ -1,7 +1,7 @@
 #!/bin/#!/usr/bin/env bash
 apt update;apt install -y git python-requests wget
 wget -q https://raw.githubusercontent.com/mmilidoni/github-downloads-count/master/gdc;chmod +x gdc
-./gdc rauldipeas radix>downloads.log ; tac downloads.log | awk 'NR <=1' | sed 's/	Total Downloads for repo rauldipeas\/radix//g' | echo `xargs expr 4000 + ` Downloads > download-counter.log
+./gdc rauldipeas radix | tac | awk 'NR <=1' | awk '{print $1}'  | echo $(xargs expr 4000 + ) Downloads > download-counter.log
 git clone https://github.com/rauldipeas/radix-website;cd radix-website
 cp ../download-counter.log website/download-counter/download-counter.log
 git config --global user.email "rauldipeas@mail.com";git config --global user.name "Raul Dipeas"
